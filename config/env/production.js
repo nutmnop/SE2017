5fdf9132f3d5efa0e61550f4d6806748e4802fe8
nutmnop <nutmnop@gmail.com>
2017-11-27 19:33:40 +0700

    fix

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
   debug: false,
-  mongoUri:'mongodb://<host>/my-project',
+  mongoUri:'mongodb://test1:test@ds157833.mlab.com:57833/rometheatre',
   sessionSecret: 'prod_secret_key'
 }
